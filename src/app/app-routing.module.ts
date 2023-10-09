a5b3f889c02bbe99e29419e4650e4ef7ef9a4728
khosro nazari <nazari@webyar.co>
2023-10-09 16:14:35 +0330

    Change Dashboard Path to dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ABaseLayoutComponent } from './Dashboard/a-base-layout/a-base-layout.component';
-import { FormControlComponent } from './Dashboard/c-forms/form-control/form-control.component';
-import { FormSelectComponent } from './Dashboard/c-forms/form-select/form-select.component';
-import { ChecksRadiosComponent } from './Dashboard/c-forms/checks-radios/checks-radios.component';
-import { InputGroupComponent } from './Dashboard/c-forms/input-group/input-group.component';
-import { FloatingLabelsComponent } from './Dashboard/c-forms/floating-labels/floating-labels.component';
-import { LayoutComponent } from './Dashboard/c-forms/layout/layout.component';
-import { ValidationComponent } from './Dashboard/c-forms/validation/validation.component';
+import { ABaseLayoutComponent } from './dashboard/a-base-layout/a-base-layout.component';
+import { FormControlComponent } from './dashboard/c-forms/form-control/form-control.component';
+import { FormSelectComponent } from './dashboard/c-forms/form-select/form-select.component';
+import { ChecksRadiosComponent } from './dashboard/c-forms/checks-radios/checks-radios.component';
+import { InputGroupComponent } from './dashboard/c-forms/input-group/input-group.component';
+import { FloatingLabelsComponent } from './dashboard/c-forms/floating-labels/floating-labels.component';
+import { LayoutComponent } from './dashboard/c-forms/layout/layout.component';
+import { ValidationComponent } from './dashboard/c-forms/validation/validation.component';
 
 
 const routes: Routes = [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ABaseLayoutComponent } from './Dashboard/a-base-layout/a-base-layout.component';
-import { BHomeComponent } from './Dashboard/b-home/b-home.component';
-import { ChecksRadiosComponent } from './Dashboard/c-forms/checks-radios/checks-radios.component';
-import { FloatingLabelsComponent } from './Dashboard/c-forms/floating-labels/floating-labels.component';
-import { FormControlComponent } from './Dashboard/c-forms/form-control/form-control.component';
-import { FormSelectComponent } from './Dashboard/c-forms/form-select/form-select.component';
-import { InputGroupComponent } from './Dashboard/c-forms/input-group/input-group.component';
-import { LayoutComponent } from './Dashboard/c-forms/layout/layout.component';
-import { ValidationComponent } from './Dashboard/c-forms/validation/validation.component';
+import { ABaseLayoutComponent } from './dashboard/a-base-layout/a-base-layout.component';
+import { BHomeComponent } from './dashboard/b-home/b-home.component';
+import { ChecksRadiosComponent } from './dashboard/c-forms/checks-radios/checks-radios.component';
+import { FloatingLabelsComponent } from './dashboard/c-forms/floating-labels/floating-labels.component';
+import { FormControlComponent } from './dashboard/c-forms/form-control/form-control.component';
+import { FormSelectComponent } from './dashboard/c-forms/form-select/form-select.component';
+import { InputGroupComponent } from './dashboard/c-forms/input-group/input-group.component';
+import { LayoutComponent } from './dashboard/c-forms/layout/layout.component';
+import { ValidationComponent } from './dashboard/c-forms/validation/validation.component';
 
 
 @NgModule({
