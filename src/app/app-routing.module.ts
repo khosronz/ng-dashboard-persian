f7a810c2df1dfbe9cdf419918c5cfd92cdadc22f
unknown <rezaabazyar@gmail.com>
2024-01-24 10:58:23 +0330

    Add tileLayer And Routing Marker

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,25 +15,28 @@ import { NavbarComponent } from './Dashboard/Components/navbar/navbar.component'
 import { OffcanvasComponent } from './Dashboard/Components/offcanvas/offcanvas.component';
 import { CollapseComponent } from './Dashboard/Components/collapse/collapse.component';
 import { OsmMapComponent } from './osm-map/osm-map.component';
+import { MapComponent } from './map/map.component';
 
-const routes: Routes = [
-    // Forms --------------------------------------------------------------
-  {path:'formcontrol',component:FormcontrolComponent},
-  {path:'home',component:HomeComponent},
-  {path:'select',component:SelectComponent},
-  {path:'check',component:CheckRadiosComponent},
-  {path:'input',component:InputComponent},
+let routes: Routes;
+routes = [
+  // Forms --------------------------------------------------------------
+  {path: 'formcontrol', component: FormcontrolComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'select', component: SelectComponent},
+  {path: 'check', component: CheckRadiosComponent},
+  {path: 'input', component: InputComponent},
   // Components --------------------------------------------------------------
-  {path:'accordion',component:AccordionComponent},
-  {path:'alert',component:AlertComponent},
-  {path:'button',component:ButtonComponent},
-  {path:'card',component:CardComponent},
-  {path:'carousel',component:CarouselComponent},
-  {path:'modal',component:ModalComponent},
-  {path:'cpllapse',component:CollapseComponent},
-  {path:'navbar',component:NavbarComponent},
-  {path:'offcanvas',component:OffcanvasComponent},
-  {path:'map',component:OsmMapComponent},
+  {path: 'accordion', component: AccordionComponent},
+  {path: 'alert', component: AlertComponent},
+  {path: 'button', component: ButtonComponent},
+  {path: 'card', component: CardComponent},
+  {path: 'carousel', component: CarouselComponent},
+  {path: 'modal', component: ModalComponent},
+  {path: 'cpllapse', component: CollapseComponent},
+  {path: 'navbar', component: NavbarComponent},
+  {path: 'offcanvas', component: OffcanvasComponent},
+  {path: 'map', component:OsmMapComponent},
+  {path: 'mapL', component:MapComponent},
 
 
 ];
--- src/app/osm-map/osm-map.component.ts
+++ src/app/osm-map/osm-map.component.ts
@@ -1,31 +1,109 @@
-import { Component, OnInit, OnDestroy, Input, Output, EventEmitter } from '@angular/core';
-import { Map, Control, DomUtil, ZoomAnimEvent , Layer, MapOptions, tileLayer, latLng } from 'leaflet';
+import { Component, AfterViewInit } from '@angular/core';
+import * as L from 'leaflet';
+// Adding Lib to globaly
+import 'leaflet-routing-machine';
+import 'leaflet-draw';
+
 @Component({
   selector: 'app-osm-map',
   standalone: false,
   templateUrl: './osm-map.component.html',
   styleUrl: './osm-map.component.scss'
 })
-export class OsmMapComponent implements OnInit, OnDestroy {
-  @Output() map$: EventEmitter<Map> = new EventEmitter;
-  @Output() zoom$: EventEmitter<number> = new EventEmitter;
-  @Input() options: MapOptions= {
-                      layers:[tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                        opacity: 0.7,
-                        maxZoom: 19,
-                        detectRetina: true,
-                        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                      })],
-                      zoom:3,
-                      center:latLng(0,0)
-  };
-  public map!: Map;
+export class OsmMapComponent implements AfterViewInit {
+
+  public map!: L.Map;
   public zoom!: number;
+
+  private locationList = [
+    { lat: 37.7195265626227, lng: 46.93359375000001, title: "تبریز", Address: "تبریز خیابان اول پلاک 1", placeType: "h" },
+    { lat: 35.60496409485937, lng: 51.50390625000001, title: "تهران", Address: "تهران خیابان اول پلاک 1", placeType: "h" },
+    { lat: 32.54851512118243, lng: 51.767578125, title: "اصفهان", Address: "اصفهان خیابان اول پلاک 1", placeType: "h" },
+    { lat: 35.783389740701296, lng: 58.88671875000001, title: "مشهد", Address: "مشهد خیابان اول پلاک 1", placeType: "h" },
+    { lat: 34.562259303839774, lng: 50.93261718750001, title: "قم", Address: "قم خیابان اول پلاک 1", placeType: "h" },
+
+    { lat: 29.231097541675027, lng: 52.99804687500001, title: "شیراز", Address: "شیراز خیابان اول پلاک 1", placeType: "r" },
+    { lat: 27.061667813752774, lng: 55.98632812500001, title: "بندرلنگه", Address: "بندرلنگه خیابان اول پلاک 1", placeType: "r" },
+    { lat: 29.11600059007595, lng: 60.51269531250001, title: "زاهدان", Address: "زاهدان خیابان اول پلاک 1", placeType: "r" },
+    { lat: 31.767357597242206, lng: 48.91113281250001, title: "اهواز", Address: "اهواز خیابان اول پلاک 1", placeType: "r" },
+    { lat: 31.767357597242206, lng: 54.31640625000001, title: "یزد", Address: "یزد خیابان اول پلاک 1", placeType: "r" }
+  ];
+
+  public latlngs = [
+    [45.51, -122.68],
+    [37.77, -122.43]
+  ];
+
+  public tileLayer1 = L.tileLayer('https://a.tile.openstreetmap.org/{z}/{x}/{y}.png', {});
 
   constructor() {
   }
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    this.map = new L.Map('map', {
+      center: [32.287, 52.954],
+      zoom: 5,
+      layers: [this.tileLayer1]
+    });
+
+    var myIcon = L.icon({
+      iconUrl: 'assets/images/1.png',
+      iconSize: [38, 38],
+      iconAnchor: [22, 94],
+      popupAnchor: [-3, -76]
+    });
+
+    //
+    // Routing
+    //
+    L.Routing.control({
+      waypoints: [
+        L.latLng(29.231097541675027, 52.99804687500001),
+        L.latLng(27.061667813752774, 55.98632812500001),
+        L.latLng(29.11600059007595, 60.51269531250001)
+      ]
+    }).addTo(this.map);
+
+
+    // Drawing Line
+    //
+    // var polyline = L.polyline(
+    //   [
+    //     [29.231097541675027, 52.99804687500001],
+    //     [27.061667813752774, 55.98632812500001],
+    //     [29.11600059007595, 60.51269531250001]
+    //   ], { color: 'red' }).addTo(this.map);
+    // this.map.fitBounds(polyline.getBounds());
+
+    var drawnItems = L.featureGroup().addTo(this.map);
+    
+    this.map.addControl(new L.Control.Draw({
+      edit: {
+        featureGroup: drawnItems
+      },
+      draw: {
+        polygon: {
+          allowIntersection: false,
+          showArea: true
+        }
+      }
+    }));
+
+    this.map.on(L.Draw.Event.CREATED, function (event) {
+      var layer = event.layer;
+
+      drawnItems.addLayer(layer);
+    });
+
+
+
+    for (let i = 0; i < this.locationList.length; i++) {
+      if (this.locationList[i].placeType === "h") {
+        L.marker([this.locationList[i].lat, this.locationList[i].lng], { icon: myIcon }).addTo(this.map);
+      } else {
+        L.marker([this.locationList[i].lat, this.locationList[i].lng], { icon: myIcon }).addTo(this.map);
+      }
+    }
   }
 
   ngOnDestroy() {
@@ -33,20 +111,8 @@ export class OsmMapComponent implements OnInit, OnDestroy {
     this.map.remove();
   };
 
-  onMapReady(map: Map) {
-    this.map = map;
-    this.map$.emit(map);
-    this.zoom = map.getZoom();
-    this.zoom$.emit(this.zoom);
-  }
-
-  onMapZoomEnd(e: any) {
-    this.zoom = e.target.getZoom();
-    this.zoom$.emit(this.zoom);
-  }
 }
 
 
-// ZoomAnimEvent
 
 
