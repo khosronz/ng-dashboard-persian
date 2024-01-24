a1ce58b2984cfe35b3fd229da554039f34bb2535
unknown <rezaabazyar@gmail.com>
2024-01-24 10:33:26 +0330

    Add tileLayer And Routing Marker

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import * as L from 'leaflet';
+
 
 @Component({
   selector: 'app-map',
@@ -8,6 +10,12 @@ import { CommonModule } from '@angular/common';
   templateUrl: './map.component.html',
   styleUrl: './map.component.scss'
 })
-export class MapComponent {
+export class MapComponent  {
+  private map : any;
+  constructor() {
+  }
+
+
+
 
 }
